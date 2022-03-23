9ca484441ad2c0922972bbaa8d2df22635d590a8
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-03-23 22:00:15 +0100

    Clean code

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './Footer.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
