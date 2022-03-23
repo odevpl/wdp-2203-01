3b24d27b70490c31df1c4ea8153edc68a483a42e
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-03-23 23:35:58 +0100

    RDW-TopBar, banner

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -1,21 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-
 import styles from './CompanyClaim.module.scss';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
 const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
-        </div>
+      <div className='row' id={styles.rowCompanyClaim}>
+        <div className='col-6 col-md-4 text-center'></div>
         <div className='col text-center'>
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
@@ -26,10 +19,14 @@ const CompanyClaim = () => (
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
             </div>
-            <div className={styles.cartCounter}>
-              <p>123456</p>
-            </div>
+            <div className={styles.cartCounter}>123456</div>
           </a>
+        </div>
+        <div className={`col-12 col-md-4 text-left ${styles.phoneNumber}`}>
+          <p>
+            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
+            222
+          </p>
         </div>
       </div>
     </div>
