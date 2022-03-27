f544914754e008ddc6411e429838fcd9affb450a
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-03-27 20:03:54 +0200

    fix conflict CompanyClaim

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -7,23 +7,14 @@ import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons
 const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
-<<<<<<< HEAD
       <div className={`row align-items-center ${styles.elements}`}>
         <div className={`col-12 text-center col-md text-md-left ${styles.phoneNumber}`}>
-=======
-      <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
->>>>>>> e981714 ( fix error RDD tablet-CompanyClaim)
           <p>
             <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
             222
           </p>
         </div>
-<<<<<<< HEAD
-        <div className='col text-center col-md'>
-=======
         <div className={`col text-center ${styles.logo}`}>
->>>>>>> e981714 ( fix error RDD tablet-CompanyClaim)
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
