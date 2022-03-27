a6b549ebe7cb99c7ef66dad6116cb9f6c0c3198a
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-03-27 19:41:02 +0200

    fix error RDD tablet-CompanyClaim

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -7,31 +7,40 @@ import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons
 const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
+<<<<<<< HEAD
       <div className={`row align-items-center ${styles.elements}`}>
         <div className={`col-12 text-center col-md text-md-left ${styles.phoneNumber}`}>
+=======
+      <div className='row align-items-center'>
+        <div className={`col text-left ${styles.phoneNumber}`}>
+>>>>>>> e981714 ( fix error RDD tablet-CompanyClaim)
           <p>
             <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
             222
           </p>
         </div>
+<<<<<<< HEAD
         <div className='col text-center col-md'>
+=======
+        <div className={`col text-center ${styles.logo}`}>
+>>>>>>> e981714 ( fix error RDD tablet-CompanyClaim)
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
         </div>
-        <div className={`col text-right col-md ${styles.cart}`}>
+        <div className={`col text-right ${styles.cart}`}>
           <a href='#' className={styles.cartBox}>
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
             </div>
             <div className={styles.cartCounter}>123456</div>
           </a>
-        </div>
-        <div className={`col-12 col-md-4 text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
+          <div className={styles.phoneNumber}>
+            <p>
+              <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560
+              - 222
+            </p>
+          </div>
         </div>
       </div>
     </div>
