d29b6ee9d231b0ca6483cfd5729a91a21143d64b
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-03-31 00:37:16 +0200

    conflicts

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -12,7 +12,7 @@ import Stars from '../Stars/Stars';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import ProductPopup from '../ProductPopup/ProductPopup';
-
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
 const ProductBox = ({
   name,
@@ -32,7 +32,6 @@ const ProductBox = ({
   addActiveClass,
   isCompare,
   isFavourite,
-
 }) => {
   const [showProdPopup, setShowProdPopup] = useState(false);
 
@@ -43,62 +42,60 @@ const ProductBox = ({
   const handleCloseProdPopup = () => setShowProdPopup(false);
 
   return (
-
-  <div className={styles.root}>
-    <div className={styles.photo}>
-    <Link to={`/product/${id}`}>
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <Link to={`/product/${id}`}>
           <img src={image} alt='lux bed' />
         </Link>
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-      <Button variant='small' onClick={handleShowProdPopup}>
-      Quick View
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant='small' onClick={handleShowProdPopup}>
+            Quick View
           </Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
+        </div>
       </div>
-      </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button
-          className={favorites ? styles.favorites : styles.outlines}
-          onClick={e => {
-            e.preventDefault();
-            favorites ? removeFromFavorites({ id }) : addToFavorites({ id });
-          }}
-          variant='outline'
-        >
-          {isFavourite && (
-            <FontAwesomeIcon className={styles.favoriteActiveIcon} icon={farHeart}>
-              Favorite
-            </FontAwesomeIcon>
-          )}
-          {!isFavourite && <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>}
-        </Button>
-        <Button
-          className={compareActive ? styles.active : styles.outlines}
-          variant='outline'
-          onClick={e => {
-            e.preventDefault();
-            if (compare.length < 4 && compareActive === false) {
-              addActiveClass({ id });
-              addToCompare({ id, name, category, price, image, stars });
-            }
-          }}
-        >
-          {isCompare && (
-            <FontAwesomeIcon className={styles.compareActive} icon={faExchangeAlt}>
-              Add to compare
-            </FontAwesomeIcon>
-          )}
-          {!isCompare && (
-            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-          )}
-      
-        </Button>
-      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button
+            className={favorites ? styles.favorites : styles.outlines}
+            onClick={e => {
+              e.preventDefault();
+              favorites ? removeFromFavorites({ id }) : addToFavorites({ id });
+            }}
+            variant='outline'
+          >
+            {isFavourite && (
+              <FontAwesomeIcon className={styles.favoriteActiveIcon} icon={farHeart}>
+                Favorite
+              </FontAwesomeIcon>
+            )}
+            {!isFavourite && <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>}
+          </Button>
+          <Button
+            className={compareActive ? styles.active : styles.outlines}
+            variant='outline'
+            onClick={e => {
+              e.preventDefault();
+              if (compare.length < 4 && compareActive === false) {
+                addActiveClass({ id });
+                addToCompare({ id, name, category, price, image, stars });
+              }
+            }}
+          >
+            {isCompare && (
+              <FontAwesomeIcon className={styles.compareActive} icon={faExchangeAlt}>
+                Add to compare
+              </FontAwesomeIcon>
+            )}
+            {!isCompare && (
+              <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+            )}
+          </Button>
+        </div>
         <div className={styles.priceWraper}>
           <div>
             {promo === 'sale' && <div className={styles.oldPrice}>$ {price} </div>}
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -9,12 +9,8 @@ import Feedback from '../../features/Feedback/Feedback';
 import CompareBar from '../../features/CompareBar/CompareBar';
 import FurnitureGallery from '../../features/FurnitureGallery/FurnitureGallery';
 import BrandsBar from '../../layout/BrandsBar/BrandsBar';
-<<<<<<< HEAD
 import BlogHomepage from '../../features/BlogHomepage/BlogHomepage';
-=======
 import Chatbox from '../../features/Chatbox/Chatbox';
-
->>>>>>> 15a4023 (Chatbox)
 
 const Homepage = () => (
   <div className={styles.root}>
